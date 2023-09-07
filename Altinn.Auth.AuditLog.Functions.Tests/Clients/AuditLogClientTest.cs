aa6563b9a6c5ad5501bd152c4089e8aded96f55f
acn-dgopa <acn-dgopa@dev-acn-tje-14>
2023-09-07 13:36:22 +0200

    Excluded contextrequest from code coverage

--- Altinn.Auth.AuditLog.Functions.Tests/Clients/AuditLogClientTest.cs
+++ Altinn.Auth.AuditLog.Functions.Tests/Clients/AuditLogClientTest.cs
@@ -2,6 +2,7 @@
 using Altinn.Auth.AuditLog.Functions.Clients.Interfaces;
 using Altinn.Auth.AuditLog.Functions.Configuration;
 using Altinn.Auth.AuditLog.Functions.Models;
+using Altinn.Auth.AuditLog.Functions.Tests.Helpers;
 using Azure.Messaging;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -90,7 +91,7 @@ namespace Altinn.Auth.AuditLog.Functions.Tests.Clients
 
             var client = new AuditLogClient(_loggerMock.Object, new HttpClient(handlerMock.Object), _platformSettings);
             // Act
-            await client.SaveAuthorizationEvent(GetAuthorizationEvent());
+            await client.SaveAuthorizationEvent(TestDataHelper.GetAuthorizationEvent());
 
             // Assert
             handlerMock.VerifyAll();
@@ -108,7 +109,7 @@ namespace Altinn.Auth.AuditLog.Functions.Tests.Clients
 
             // Act
 
-            await Assert.ThrowsAsync<HttpRequestException>(async () => await client.SaveAuthorizationEvent(GetAuthorizationEvent()));
+            await Assert.ThrowsAsync<HttpRequestException>(async () => await client.SaveAuthorizationEvent(TestDataHelper.GetAuthorizationEvent()));
 
             // Assert
             handlerMock.VerifyAll();
@@ -142,105 +143,6 @@ namespace Altinn.Auth.AuditLog.Functions.Tests.Clients
                   _loggerMock.Object,
                   new HttpClient(messageHandlerMock),
                   _platformSettings);
-        }
-
-        private static AuthorizationEvent GetAuthorizationEvent()
-        {
-            Models.Attribute userAttribute = new Models.Attribute()
-            {
-                Id = "urn:altinn:userid",
-                Value = "1",
-            };
-
-            Models.Attribute role1Attribute = new Models.Attribute()
-            {
-                Id = "urn:altinn:role",
-                Value = "dagl",
-            };
-
-            Models.Attribute role2Attribute = new Models.Attribute()
-            {
-                Id = "urn:altinn:role",
-                Value = "utinn",
-            };
-
-           Models.Attribute actionAttribute = new Models.Attribute()
-            {
-                Id = "urn:oasis:names:tc:xacml:1.0:action:action-id",
-                Value = "read",
-                DataType = "http://www.w3.org/2001/XMLSchema#string"
-            };
-
-            Models.Attribute instanceAttribute = new Models.Attribute()
-            {
-                Id = "urn:altinn:instance-id",
-                Value = "1000/26133fb5-a9f2-45d4-90b1-f6d93ad40713",
-                IncludeInResult = true
-            };
-            Models.Attribute orgAttribute = new Models.Attribute()
-            {
-                Id = "urn:altinn:org",
-                Value = "skd"
-            };
-            Models.Attribute appAttribute = new Models.Attribute()
-            {
-                Id = "urn:altinn:app",
-                Value = "taxreport"
-            };
-            Models.Attribute partyAttribute = new Models.Attribute()
-            {
-                Id = "urn:altinn:partyid",
-                Value = "1000"
-            };
-            Models.Attribute taskAttribute = new Models.Attribute()
-            {
-                Id = "urn:altinn:task",
-                Value = "formfilling"
-            };
-
-            AccessSubject accessSubject = new AccessSubject();
-            accessSubject.Attribute = new List<Models.Attribute>();
-            accessSubject.Attribute.Add(userAttribute);
-            accessSubject.Attribute.Add(role1Attribute);
-            accessSubject.Attribute.Add(role2Attribute);
-
-            Models.Action action = new Models.Action();
-            action.Attribute = new List<Models.Attribute>();
-            action.Attribute.Add(actionAttribute);
-
-            Models.Resource resource = new Models.Resource();
-            resource.Attribute = new List<Models.Attribute>();
-            resource.Attribute.Add(instanceAttribute);
-            resource.Attribute.Add(orgAttribute);
-            resource.Attribute.Add(appAttribute);
-            resource.Attribute.Add(partyAttribute);
-            resource.Attribute.Add(taskAttribute);
-
-
-            ContextRequest contextRequest = new ContextRequest();
-            contextRequest.AccessSubject = new List<AccessSubject>();
-            contextRequest.Action = new List<Models.Action>();
-            contextRequest.AccessSubject = new List<AccessSubject>();
-            contextRequest.Resources = new List<Resource>();
-            contextRequest.AccessSubject.Add(accessSubject);
-            contextRequest.Action.Add(action);
-            contextRequest.Resources.Add(resource);
-
-            AuthorizationEvent authorizationEvent = new AuthorizationEvent()
-            {
-                SubjectUserId = "2000000",
-                SubjectParty = "",
-                ResourcePartyId = "1000",
-                Resource = "taxreport",
-                InstanceId = "1000/26133fb5-a9f2-45d4-90b1-f6d93ad40713",
-                Operation = "read",
-                TimeToDelete = "",
-                IpAdress = "192.0.2.1",
-                ContextRequestJson = contextRequest
-
-            };
-
-            return authorizationEvent;
-        }
+        }        
     }
 }
--- src/Functions/Altinn.Auth.AuditLog.Functions/Models/ContextRequest.cs
+++ src/Functions/Altinn.Auth.AuditLog.Functions/Models/ContextRequest.cs
@@ -1,26 +1,30 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Altinn.Auth.AuditLog.Functions.Models
 {
     /// <summary>
     /// This model describes an authorization event. An authorization event is an action triggered when a user requests access to an operation
     /// </summary>
-  
+    [ExcludeFromCodeCoverage]
     public class AccessSubject
     {
         public List<Attribute> Attribute { get; set; }
     }
 
+    [ExcludeFromCodeCoverage]
     public class Action
     {
         public List<Attribute> Attribute { get; set; }
     }
 
+    [ExcludeFromCodeCoverage]
     public class Resource
     {
         public List<Attribute> Attribute { get; set; }
     }
 
+    [ExcludeFromCodeCoverage]
     public class Attribute
     {
         public string Id { get; set; }
@@ -29,6 +33,7 @@ namespace Altinn.Auth.AuditLog.Functions.Models
         public bool IncludeInResult { get; set; }
     }
 
+    [ExcludeFromCodeCoverage]
     public class ContextRequest
     {
         public bool ReturnPolicyIdList { get; set; }
