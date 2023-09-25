7fb4c2ff3bb614e8e11d6619a86f6d6d2da3f53d
acn-dgopa <acn-dgopa@dev-acn-tje-14>
2023-09-25 14:54:11 +0200

    removed commented code

--- Altinn.Auth.AuditLog.Functions.Tests/Functions/AuthorizationEventsProcessorTest.cs
+++ Altinn.Auth.AuditLog.Functions.Tests/Functions/AuthorizationEventsProcessorTest.cs
@@ -21,25 +21,6 @@ namespace Altinn.Auth.AuditLog.Functions.Tests.Functions
         [Fact]
         public async Task Run_ConfirmDeserializationOfAuthorizationEvent()
         {
-
-            // Arrange
-            //string serializedAuthorizationEvent = "{" +
-            //    "\"TimeStamp\":\"0001-01-01T00:00:00\",\"SubjectUserId\":\"2000000\",\"SubjectParty\":\"\"," +
-            //    "\"ResourcePartyId\":\"1000\",\"Resource\":\"taxreport\",\"InstanceId\":\"1000/26133fb5-a9f2-45d4-90b1-f6d93ad40713\"," +
-            //    "\"Operation\":\"read\",\"TimeToDelete\":\"\",\"IpAdress\":\"192.0.2.1\"," +
-            //    "\"ContextRequestJson\":{\"ReturnPolicyIdList\":false," +
-            //    "\"AccessSubject\":[{\"Attribute\":[{\"Id\":\"urn:altinn:userid\",\"Value\":\"1\",\"DataType\":null,\"IncludeInResult\":false}," +
-            //    "{\"Id\":\"urn:altinn:role\",\"Value\":\"dagl\",\"DataType\":null,\"IncludeInResult\":false}," +
-            //    "{\"Id\":\"urn:altinn:role\",\"Value\":\"utinn\",\"DataType\":null,\"IncludeInResult\":false}]}]," +
-            //    "\"Action\":[{\"Attribute\":[{\"Id\":\"urn:oasis:names:tc:xacml:1.0:action:action-id\",\"Value\":\"read\"," +
-            //    "\"DataType\":\"http://www.w3.org/2001/XMLSchema#string\",\"IncludeInResult\":false}]}]," +
-            //    "\"Resources\":[{\"Attribute\":[{\"Id\":\"urn:altinn:instance-id\",\"Value\":\"1000/26133fb5-a9f2-45d4-90b1-f6d93ad40713\"," +
-            //    "\"DataType\":null,\"IncludeInResult\":true},{\"Id\":\"urn:altinn:org\",\"Value\":\"skd\"," +
-            //    "\"DataType\":null,\"IncludeInResult\":false},{\"Id\":\"urn:altinn:app\",\"Value\":\"taxreport\"," +
-            //    "\"DataType\":null,\"IncludeInResult\":false},{\"Id\":\"urn:altinn:partyid\",\"Value\":\"1000\"," +
-            //    "\"DataType\":null,\"IncludeInResult\":false},{\"Id\":\"urn:altinn:task\",\"Value\":\"formfilling\"," +
-            //    "\"DataType\":null,\"IncludeInResult\":false}]}]}}\r\n";
-
             string serializedAuthorizationEvent = "{" +
                 "\"TimeStamp\":\"0001-01-01T00:00:00\",\"SubjectUserId\":\"\",\"SubjectOrgCode\":\"skd\",\"SubjectOrgNumber\":\"\",\"SubjectParty\":\"\"," +
                 "\"ResourcePartyId\":\"1000\",\"Resource\":\"taxreport\",\"InstanceId\":\"1000/26133fb5-a9f2-45d4-90b1-f6d93ad40713\"," +
