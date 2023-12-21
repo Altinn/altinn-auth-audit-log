45ceb412bb11abb7f796b9d8520237e904d55e8b
acn-dgopa <acn-dgopa@dev-acn-tje-14>
2023-12-21 10:36:59 +0100

    Added health endpoint and application insights configuration

---
+++ src/Altinn.Auth.AuditLog.Core/Models/KeyVaultSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altinn.Auth.AuditLog.Core.Models
+{
+    /// <summary>
+    /// General configuration settings
+    /// </summary>
+    public class KeyVaultSettings
+    {
+        /// <summary>
+        /// Gets or sets the secret uri
+        /// </summary>
+        public string SecretUri { get; set; }
+    }
+}
