fd9bae958b3403c7e61d061a41edd99a2fcb5401
Dhanalakshmi Gopalswamy <34273718+acn-dgopa@users.noreply.github.com>
2023-12-27 13:42:24 +0100

    Added health endpoint and application insights configuration (#60)
    * Added health endpoint and application insights configuration
    
    * Added health check test
    
    ---------
    
    Co-authored-by: acn-dgopa <acn-dgopa@dev-acn-tje-14>

---
+++ src/test/Altinn.Auth.AuditLog.Tests/Health/HealthCheckTests.cs
@@ -0,0 +1,57 @@
+﻿using Altinn.Auth.AuditLog.Health;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altinn.Auth.AuditLog.Tests.Health
+{
+    /// <summary>
+    /// Health check 
+    /// </summary>
+    public class HealthCheckTests : IClassFixture<CustomWebApplicationFactory<HealthCheck>>
+    {
+        private readonly CustomWebApplicationFactory<HealthCheck> _factory;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="fixture">The web application fixture</param>
+        public HealthCheckTests(CustomWebApplicationFactory<HealthCheck> fixture)
+        {
+            _factory = fixture;
+        }
+
+        /// <summary>
+        /// Verify that component responds on health check
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task VerifyHealthCheck_OK()
+        {
+            HttpClient client = GetTestClient();
+
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "/health");
+
+            HttpResponseMessage response = await client.SendAsync(httpRequestMessage);
+            string content = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        private HttpClient GetTestClient()
+        {
+            HttpClient client = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                });
+            }).CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = false });
+
+            return client;
+        }
+    }
+}
