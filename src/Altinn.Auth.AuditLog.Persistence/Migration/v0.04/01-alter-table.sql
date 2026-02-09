422b4dce8fdfba893708055f8768295fbdee9dab
Dhanalakshmi Gopalswamy <acn-dgopa@ai-dev.no>
2026-02-09 11:23:32 +0100

    Script to add new authentication methods in DB

---
+++ src/Altinn.Auth.AuditLog.Persistence/Migration/v0.04/01-alter-table.sql
@@ -0,0 +1,11 @@
+ALTER TABLE authentication.authenticationmethod
+
+-- Add new authentication methods
+INSERT INTO authentication.authenticationmethod (authenticationmethodid, name, description)
+VALUES (21, 'MinIDTOTP', 'MinIDTOTP')
+ON CONFLICT (authenticationmethodid) DO NOTHING;
+
+INSERT INTO authentication.authenticationmethod (authenticationmethodid, name, description)
+VALUES (22, 'IdportenEpost', 'IdportenEpost')
+ON CONFLICT (authenticationmethodid) DO NOTHING;
+
