8cb93a43559e9a846b2d201453285bb49e3b511b
Andreas Isnes <andreas.isnes@gmail.com>
2026-09-09 13:49:27 +0200

    add trace id for authz

--- src/Altinn.Auth.AuditLog.Persistence/Migration/v0.05/01-add-trace-id.sql
+++ src/Altinn.Auth.AuditLog.Persistence/Migration/v0.05/01-add-trace-id.sql
@@ -1,3 +1,6 @@
--- Add new authentication methods
+-- Add field trace_id
 ALTER TABLE authz.eventlogv1
-ADD trace_id VARCHAR(255);
+ADD trace_id VARCHAR(63) NULL;
+
+-- Add index for trace id
+CREATE INDEX authz_eventlogv1_trace_id_idx ON authz.eventlogv1 (trace_id);
