27d82fa6308511453a11640694a52fb53d04a662
Andreas Isnes <andreas.isnes@gmail.com>
2026-09-10 14:28:46 +0200

    drop idx for trace id iff exists

--- src/Altinn.Auth.AuditLog.Persistence/Migration/v0.05/01-add-trace-id.sql
+++ src/Altinn.Auth.AuditLog.Persistence/Migration/v0.05/01-add-trace-id.sql
@@ -2,5 +2,4 @@
 ALTER TABLE authz.eventlogv1
 ADD trace_id VARCHAR(63) NULL;
 
--- Add index for trace id
 CREATE INDEX authz_eventlogv1_trace_id_idx ON authz.eventlogv1 (trace_id);
---
+++ src/Altinn.Auth.AuditLog.Persistence/Migration/v0.06/01-drop-trace-id-idx-if-exist.sql
@@ -0,0 +1,1 @@
+DROP INDEX IF EXISTS authz.authz_eventlogv1_trace_id_idx;
